f3a9b3afac3d3a2c809debbe732d2ce09397e562
Aryansh <aryanshs@buffalo.edu>
2024-10-05 21:28:03 -0400

    ui for experiments page

--- visualize-client/src/components/Experiments.js
+++ visualize-client/src/components/Experiments.js
@@ -1,10 +1,50 @@
-import React, { useState } from "react";
-import { fetchStudyData } from "../services/appService";
+import React from "react";
+import "../styles/Experiments.css"; // Ensure this path is correct
 
 export default function Experiments() {
   return (
     <div className="experimentsContainer bodyScreenFit">
-      <h1>Experiments</h1>
+      {/* image background with an overlay */}
+      <div className="experimentSolarImageContainer">
+        <div className="imageOverlay"></div> {/* Semi-transparent overlay */}
+        <div className="experimentsHeadingContainer">
+          <h1>Explore</h1>
+          <h1>Experiments</h1>
+          <div className="experimentSearchBar">
+            <input type="text" placeholder="Search experiments Coming Soon.." />
+          </div>
+        </div>
+      </div>
+
+      <div className="experimentsStudyCardsContainer">
+        <div className="experimentsStudyCard card379">
+          <div className="experimentsHeadingBox">
+            <h2>OSD-379</h2>
+          </div>
+          <div className="experimentsDescriptionBox">
+            <p>
+              Transcriptional profiling of livers from mice flown on Rodent
+              Research Reference Mission-1 (RRRM-1)
+            </p>
+          </div>
+
+          <button>View Details</button>
+        </div>
+        <div className="experimentsStudyCard card665">
+          <div className="experimentsHeadingBox">
+            <h2>OSD-665</h2>
+          </div>
+          <div className="experimentsDescriptionBox">
+            <p>
+              {" "}
+              Transcriptional profiling of right extensor digitorum longus
+              muscle from mice flown on the RR-23 mission
+            </p>
+          </div>
+
+          <button>View Details</button>
+        </div>
+      </div>
     </div>
   );
 }
