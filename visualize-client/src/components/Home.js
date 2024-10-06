b8cf72c10c0f6759749ff4ed2af2ddb4b8c43a63
Braxton-207 <myuen1860@gmail.com>
2024-10-06 15:37:25 -0400

    saving home page

--- visualize-client/src/components/Home.js
+++ visualize-client/src/components/Home.js
@@ -1,11 +1,54 @@
 import React, { useState } from "react";
 import { fetchStudyData } from "../services/appService";
 import "../App.css";
+import BannerImage from "../images/homepage-mouse.jfif"
+import { Link } from "react-router-dom";
+import "../styles/Home.css";
 
 export default function Home() {
   return (
     <div className="homeContainer bodyScreenFit">
-      <h1>Home</h1>
+
+
+
+    <div className= 'left'>
+
+      <div className= 'header'>
+      <h1 className ="home-heading">
+      Visualize Biological <br />
+      Experiments in Space <br />
+
+
+      </h1>
+      </div>
+
+      <div className= 'paragraph'>
+      <p1 className = "home-text"> 
+      Bringing complex biological space data to life through compelling visualizations.</p1>
+      </div>
+
+      <div className= 'experiment'>
+      <Link to="/Experiments" className="button1">
+        Explore Experiments 
+      </Link>
+      </div>
+
+      <div className= 'about'>
+      <Link to="/About" className="button2">
+        About
+        </Link>
+        </div>
+
+      </div>
+
+    <div className= "right">
+      <div className = "homeImage">
+        <img src = {BannerImage} alt="mouse in space.img"/>
+
+      </div>
     </div>
+
+
+    </div>  
   );
 }
